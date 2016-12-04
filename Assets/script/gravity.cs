6f2cc2a40f95c156caf6f81f80e5743aead809ad
DESKTOP-4TC8FTH\ravrik <register3510@gmail.com>
2016-12-04 12:11:02 +0900

    アセット導入

---
+++ Assets/script/gravity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravity : MonoBehaviour
+{
+
+    //   const float grav_amou = 9.80665;
+    public GameObject planet;       // 引力の発生する星
+    public float accelerationScale; // 加速度の大きさ
+
+    void FixedUpdate()
+    {
+        // 星に向かう向きの取得
+        var direction = planet.transform.position - transform.position;
+        direction.Normalize();
+
+        GetComponent<Rigidbody>().AddForce(accelerationScale * direction, ForceMode.Acceleration);
+    }
+}

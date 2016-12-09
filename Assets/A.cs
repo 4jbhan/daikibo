4bd77409df7874385fb74039a95a6705b8bf7926
DESKTOP-EURE4VS\regis <register3510@gmail.com>
2016-12-09 23:40:56 +0900

    instance関係

---
+++ Assets/A.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class A : MonoBehaviour {
+    public int z = 9;
+    public float[,] x = new float[8, 8];
+    public Vector3[,] y = new Vector3[8, 8];
+
+    public void possetter()
+    {
+        x[0,0] = 0;
+    }
+}
---
+++ Assets/B.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class B : MonoBehaviour {
+    public A a;
+    void Start() {
+        a = GetComponent<A>();
+        a.z = 0;
+        a.possetter();
+        a.x[5,6] = (float)1;
+    }
+}
